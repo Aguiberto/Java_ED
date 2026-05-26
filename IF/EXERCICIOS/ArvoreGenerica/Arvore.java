17c23ba5f48e4a1471c43656ca05b3cfba0fcada
unknown <aguibertocandido@gmail.com>
2026-05-26 08:42:25 -0300

    feat: método elements

--- IF/EXERCICIOS/ArvoreGenerica/Arvore.java
+++ IF/EXERCICIOS/ArvoreGenerica/Arvore.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Arvore{
 
    private NoArvore raiz;
@@ -59,6 +63,38 @@ public class Arvore{
 
       //Soma mais um a cada nó percorrido
       return 1+maiorAlturaFilhos;
+   }
+
+   public boolean isEmpty(){
+      
+      return tamanho == 0;
+   }
+
+   // Retorna um Iterador que possibilida que o usuário possa navegar pela árvore
+   // Dessa forma o usuário pode saber todos os elementos de uma árvore ou qualquer outra estrutura.
+   public Iterator elements(){
+
+      // lista para guardar os elementos da árvores
+      List<Object> listaElementos = new ArrayList<>();
+
+      // Método auxiliar para preOrdemElementoscher a lista usando Pré-ordem
+      preOrdemElementos(this.raiz, listaElementos);
+
+      return listaElementos.iterador();
+   }
+
+   private void preOrdemElementos(NoArvore no , List<Object> lista){
+
+      if(no == null){
+         return;
+      }
+
+      lista.add(no.get());
+
+      for(NoArvore filho: no.getFilhos()){
+         preOrdemElementos(filho,lista);
+      }
+
    }
 
 
