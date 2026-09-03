d10a48b577c9e78eb1adb2292c5ab4e0d1d81c70
unknown <aguibertocandido@gmail.com>
2026-09-03 15:01:14 -0300

    métodos auxiliares para remover um nó

--- IF/EDNL/ATIVIDADES/AVL/ArvoreBB.java
+++ IF/EDNL/ATIVIDADES/AVL/ArvoreBB.java
@@ -67,61 +67,10 @@ public class ArvoreBB{
         }
     }
 
-    public Object remove(Node noRemovido){
+    public Object remove(Object valorNo){
 
-        /*
-            Condições
-            0. Buscar o nó
-
-            1. Remover um nó folha:
-                Apenas atribuir null ao valor
-            
-            2. Remover um nó com um filho:
-                Fazer mudanças de referências da seguinte maneira:
-                O filho assume o lugar do pai
-                O filho passa a ser filho do avô
-                O avô passa a ser pai do filho
-
-            3. Remover um nó com 2 filhos
-                Fazer mudança de referências da seguinte maneira:
-                Buscar o sucessor (Menor valor entre os maiores valores)
-                Sucessos recebe as referências do valor removido
-                Valor a ser removido tem referência para pai como null
-                Valor a ser removido tem recebe valor null
-         */
-
-        Node atual = raiz;
-        Node noAlvo = noRemovido
-
-        while(atual != noAlvo){
-
-            // encontrou o nó
-            if(atual == noAlvo){
-
-                // é um nó folha
-                if(atual.getFilhoDireito() == null && atual.getFilhoEsquerdo() == null){
-                    Node removido = atual;
-                    atual.setValor(null);
-                    atual.setPai(null);
-                    return removido;
-
-                // tem um filho
-                }else if(atual.getFilhoDireito() != null || atual.getFilhoEsquerdo() != null){
-                
-                                       
-
-                // tem dois filhos
-                }else{
-
-                }
-
-
-            }else{
-                // Nó não foi encontrado
-                return;
-            }
-
-        }
+        Node removido = buscar(valorNo);
+        if()
 
     }
 
@@ -141,6 +90,124 @@ public class ArvoreBB{
     
     public Node criarNo(Object valor){
         return Node novoNo = new Node(valor);
+    }
+
+    public Node buscar(Object valor){
+
+        Node atual = this.raiz;
+        Comparable alvo = (Comparable) valor;
+
+        while( atual != null){
+
+            Comparable valorAtual = (Comparable) atual.getValor();
+            int comparacao = alvo.compareTo(valorAtual);
+
+            
+            if(comparacao == 0){
+
+                return atual;
+                // encontrou o valor
+
+            }else if(comparacao < 0){
+                
+                atual = atual.getFilhoEsquerdo();
+                // o valor passado é menor
+
+            }else{
+
+                atual = atual.getFilhoDireito();
+                // o valor passado e maior
+            }
+
+            // Se não encontrar nada
+            return null;
+
+        }   
+
+    }
+
+
+    public Node obterSucessor(Node no){
+
+        if(no == null || no.getFilhoDireito() == null){
+            return null;
+        }
+
+        Node sucessor = no.getFilhoDireito();
+        while(sucessor.getFilhoEsquerdo() != null){
+
+            sucessor = sucessor.getFilhoEsquerdo();
+
+        }
+
+        return sucessor;
+
+    }
+
+    public Object removerNo(Node no){
+
+            /*
+                                Condições
+            0. Buscar o nó
+
+            1. Remover um nó folha:
+                Apenas atribuir null ao valor
+            
+            2. Remover um nó com um filho:
+                Fazer mudanças de referências da seguinte maneira:
+                O filho assume o lugar do pai
+                O filho passa a ser filho do avô
+                O avô passa a ser pai do filho
+
+            3. Remover um nó com 2 filhos
+                Busca o sucessor
+                Substitui o valor do nó a ser removido pelo valor do sucessor
+                Remove fisicamento o sucessor (mais simples)
+         */
+
+        // Caso 3 : No com 2 filhos
+        if (no.getFilhoDireito() != null && no.getFilhoEsquerdo() != null){
+
+            Node sucessor = obterSucessor(no);
+            no.setValor(sucessor.getValor());
+            removerNo(sucessor);        
+        } 
+
+        Node filho = (no.getFilhoEsquerdo() != null) ? no.getFilhoEsquerdo : no.getFilhoDireito();
+        Node pai = no.getPai();
+
+
+        if (filho != null){
+            filho.setPai(pai);
+            // o pai do atributo é o avô do filho
+            // esse comando faz a conexão do filho com o avô
+            // "Remove o pai"
+        }
+
+        // Caso 1 : No folha
+        if( pai == null){
+            this.raiz = filho;
+            // esse nó era a raiz
+            // o filho dele toma o lugar dele
+            // a ele( a raiz) é removido e o filho se torna o novo riz
+
+        //Caso 2a : no com 1 filho
+        }else if(no == pai.getFilhoEsquerdo()){
+        //caso com 1 filho sendo esse filho o esquerdo
+
+            // é o filho esquerdo?
+            pai.setFilhoEsquerdo(filho);
+            // o neto passa ser o filho esquerdo
+
+        // Caso 2b: no com 1 filho
+        }else{
+        //caso com 1 filho sendo esse filho o direito
+
+            pai.setFilhoDireito(filho);
+            // neto passa ser o filho direito
+        }
+
+
     }
 
 }
